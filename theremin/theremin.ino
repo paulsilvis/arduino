94999ffef12d91e5cdd49201b786f8ebb11b99ac
paulsilvis <paulsilvis@comcast.net>
2018-12-27 15:39:51 -0500

    actually works, kinda

--- theremin/theremin.ino
+++ theremin/theremin.ino
@@ -22,9 +22,11 @@ void setup() {
   pinMode(piezoPin, OUTPUT);
   pinMode(triggerPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  noTone(piezoPin);
+
 }
 
-static void readToneSensor(int&& cm, int&& inches)
+static void readToneSensor(long* cm, long* inches)
 { 
   long duration;
   
@@ -39,29 +41,43 @@ static void readToneSensor(int&& cm, int&& inches)
   // Read the signal from the sensor: a HIGH pulse whose
   // duration is the time (in microseconds) from the sending
   // of the ping to the reception of its echo off of an object.
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
  
+  duration = pulseIn(echoPin, HIGH);
+  Serial.print(duration);
+  Serial.println();
   // Convert the time into a distance
-  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
-  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
+  *cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  *inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
+  Serial.print(*cm);
+  Serial.println();
 }
 
 
 void loop() {
+  static long prevCm = 0;
   long cm, inches;
+  double fc = 0.05;
+  static double prevVal = 0.0;
   
-  readToneSensor(cm, inches);
-  if (cm > 10 && cm < 40) {
-    tone(piezoPin, cm*100);
+  readToneSensor(&cm, &inches);
+  if (cm > 10 && cm < 140) {
+    //tone(piezoPin, cm*25);
+    double freq = cm*15;
+    freq = freq * fc + (1.0-fc)*prevVal;
+    prevVal = freq;
+    tone(piezoPin, long(freq));
+    Serial.print(freq);
+    Serial.println();
   } else {
     noTone(piezoPin);
   }
+  
   Serial.print(inches);
   Serial.print("in, ");
   Serial.print(cm);
   Serial.print("cm");
   Serial.println();
   
-  delay(250);
+  //delay(250);
+
 }
