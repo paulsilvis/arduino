97dea327ba0d1b4037650a029c3ac7f0d314c6ea
paulsilvis <paulsilvis@comcast.net>
2018-12-26 19:34:25 -0500

    Add files via upload

--- theremin/theremin.ino
+++ theremin/theremin.ino
@@ -15,7 +15,6 @@ int piezoPin = 8;
  
 int triggerPin = 11;    // Trigger
 int echoPin = 12;    // Echo
-long duration, cm, inches;  // duration in milliseconds
 
 void setup() {
   Serial.begin (9600);
@@ -25,8 +24,10 @@ void setup() {
   pinMode(echoPin, INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+static void readDistanceSensor(int&& cm, int&& inches)
+{ 
+  long duration;
+  
   // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
   digitalWrite(triggerPin, LOW);
@@ -44,6 +45,13 @@ void loop() {
   // Convert the time into a distance
   cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
   inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
+}
+
+
+void loop() {
+  long cm, inches;  // duration in milliseconds
+  
+  readDistanceSensor(cm, inches);
 
   Serial.print(inches);
   Serial.print("in, ");
